aa94ef5698d9e913a2e9121f0f677121e9ea9e7f
Maddie <madwester@gmail.com>
2018-09-10 16:27:15 +1000

    commiting to new repo

---
+++ js/carousel.js
@@ -0,0 +1,41 @@
+var main = function() {
+
+	var paused = false
+
+	$('.arrowR').click(function() {
+        event.preventDefault();
+		paused = true;
+		$('#slideshow > div:first')
+		.fadeOut(1000)
+		.next()
+		.fadeIn(1000)
+		.end()
+		.appendTo('#slideshow');
+	});
+		
+	$('.arrowL').click(function() {
+        event.preventDefault();
+		paused = true;
+		$('#slideshow > div:last')
+		.fadeIn(1000)
+		.prependTo('#slideshow')
+		.next()
+		.fadeOut(1000)
+		.end();
+	});
+
+	setInterval(function() {
+		if (paused === false) { 
+			$('#slideshow > div:first')
+			.fadeOut(1000)
+			.next()
+			.fadeIn(1000)
+			.end()
+			.appendTo('#slideshow');
+		};
+	}, 3000);
+   
+	
+};
+
+$(document).ready(main);
